bb27fdacad1adeee88609ebd4c268d3b130375c2
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-29 14:47:47 +0300

    Setup splash fragment handler and navigate screen

--- Foody/app/src/main/java/com/yagmurerdogan/foody/SplashFragment.kt
+++ Foody/app/src/main/java/com/yagmurerdogan/foody/SplashFragment.kt
@@ -1,17 +1,29 @@
 package com.yagmurerdogan.foody
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.yagmurerdogan.foody.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+        },3000)
+
+        binding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+
+        return binding.root
     }
 }
