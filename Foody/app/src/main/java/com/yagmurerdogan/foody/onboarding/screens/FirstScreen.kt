018672a40b89930db516e70f9704ba26eceb27d8
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-29 16:01:00 +0300

    Import viewpager2 instead of vievpager

--- Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/screens/FirstScreen.kt
+++ Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/screens/FirstScreen.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.yagmurerdogan.foody.R
 import com.yagmurerdogan.foody.databinding.FragmentFirstScreenBinding
 
@@ -21,7 +20,7 @@ class FirstScreen : Fragment() {
 
         binding = FragmentFirstScreenBinding.inflate(layoutInflater, container, false)
 
-        val viewPager = activity?.findViewById<ViewPager>(R.id.viewPager)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
         binding.next.setOnClickListener {
             viewPager?.currentItem = 1
--- Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/screens/SecondScreen.kt
+++ Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/screens/SecondScreen.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.yagmurerdogan.foody.R
 import com.yagmurerdogan.foody.databinding.FragmentSecondScreenBinding
 
@@ -20,7 +20,7 @@ class SecondScreen : Fragment() {
 
         binding = FragmentSecondScreenBinding.inflate(layoutInflater, container, false)
 
-        val viewPager = activity?.findViewById<ViewPager>(R.id.viewPager)
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
 
         binding.next2.setOnClickListener {
             viewPager?.currentItem = 2
