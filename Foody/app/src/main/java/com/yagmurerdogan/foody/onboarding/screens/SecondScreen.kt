f7a06776f0b5652287a2f6fa5f791a384f2f6d56
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-29 14:16:31 +0300

    Create second screen layout and class

---
+++ Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/screens/SecondScreen.kt
@@ -0,0 +1,19 @@
+package com.yagmurerdogan.foody.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yagmurerdogan.foody.R
+
+class SecondScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second_screen, container, false)
+    }
+
+}
