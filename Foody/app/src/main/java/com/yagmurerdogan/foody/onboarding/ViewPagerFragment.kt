309e5e35ccd67478cc5912d5cbbad87a2a899081
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-29 13:53:35 +0300

    Create view pager fragment class and layout

---
+++ Foody/app/src/main/java/com/yagmurerdogan/foody/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,19 @@
+package com.yagmurerdogan.foody.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yagmurerdogan.foody.R
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+    }
+
+}
